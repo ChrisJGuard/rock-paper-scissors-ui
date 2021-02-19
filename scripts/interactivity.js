223c611de9e7910289315c6b84ad9da18b90af44
Chris Guard <christopher.guard@gmail.com>
2021-02-19 18:32:08 +0000

    Clean interactivity script

--- scripts/interactivity.js
+++ scripts/interactivity.js
@@ -1,9 +1,15 @@
-const userBtn = document.querySelectorAll(".user-button");
-userBtn.forEach((button) => {
+// Select all buttons for toggle styling
+const button = document.querySelectorAll(".user-button");
+
+// Cycle through buttons and add event listeners
+button.forEach(toggleSelected);
+
+// Adds mousedown and mouseup class toggles
+function toggleSelected(button) {
   button.addEventListener("mousedown", () => {
     button.classList.toggle("selected");
   });
   button.addEventListener("mouseup", () => {
     button.classList.toggle("selected");
   });
-});
+}
